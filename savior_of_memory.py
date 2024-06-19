6a0abc4659b01ee5112d48cfc149e55a0665faf2
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-06-19 17:01:48 -0500

    Added function to truncate svd

--- savior_of_memory.py
+++ savior_of_memory.py
@@ -17,8 +17,29 @@ def svd(A: np.array, error_tolerance: float=10**-50):
     #Keep only the corresponding eigenvectors
     positive_eigenvectors = np.array([eigenvectors[i] for i in range(rank)])
 
-    #TODO: Construct U with array broadcasting
+    #Construct U with array broadcasting
+    U = np.dot(A, np.dot(positive_eigenvectors, np.diag(1/positive_singular_values)))
 
-    return singular_values, eigenvectors, positive_singular_values, positive_eigenvectors
+    return U, positive_singular_values, positive_eigenvectors.conj().T
 
-print(svd(np.array([[1, 2], [3, 4]])))
+def truncated_svd(A: np.array, s: int):
+    U, singular_values, VH = svd(A)
+
+    #U_truncated should be m x s, V_truncated should be n x s, and sigma_truncated should be s x s
+    U_truncated = U[:, :s]
+    singular_values_truncated = singular_values[:s]
+    V_truncated = VH[:, :s]
+    return U_truncated, singular_values_truncated, V_truncated.conj().T
+
+A = np.array([[1, 2, 3], [3, 4, 5]])
+U, singular_values, VH = svd(A)
+print(f"U: \n{U}")
+print(f"Singular Values: \n{singular_values}")
+print(f"VH: \n{VH}")
+print(f"Original Matrix: \n{np.dot(U, np.dot(np.diag(singular_values), VH))}")
+
+U_truncated, singular_values_truncated, VH_truncated = truncated_svd(A, 2)
+print(f"U_truncated: \n{U_truncated}")
+print(f"Singular Values Truncated: \n{singular_values_truncated}")
+print(f"VH_truncated: \n{VH_truncated}")
+print(f"Reconstructed Matrix: \n{np.dot(U_truncated, np.dot(np.diag(singular_values_truncated), VH_truncated))}")
