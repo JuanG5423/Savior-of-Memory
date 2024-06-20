d5affde9fbed5d750f769a204b8748218978c7cb
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-06-19 19:50:01 -0500

    Attempted to add function to compress image

--- savior_of_memory.py
+++ savior_of_memory.py
@@ -1,9 +1,14 @@
 import numpy as np
-from scipy import linalg as la
+import sys
+import scipy.linalg as la
+from imageio.v2 import imread
+from matplotlib import pyplot as plt
+from PIL import Image
 
 def svd(A: np.array, error_tolerance: float=10**-50):
-    #Get the eigenvalues and eigenvectors of (A^T)A
-    eigenvalues, eigenvectors = np.linalg.eig(np.dot(A.conj().T, A))
+    #Get the eigenvalues and eigenvectors of (A^H)A
+    eigenvalues, eigenvectors = la.eigh(np.dot(A.conj().T, A))
+    print(min(eigenvalues), max(eigenvalues))
     
     #Calculate the singular values
     singular_values = eigenvalues ** 0.5
@@ -12,10 +17,10 @@ def svd(A: np.array, error_tolerance: float=10**-50):
     rank = len([singular_value for singular_value in singular_values if singular_value > error_tolerance])
 
     #Keep only the positive singular values
-    positive_singular_values = np.array([singular_values[i] for i in range(rank)])
+    positive_singular_values = singular_values[:rank]
 
     #Keep only the corresponding eigenvectors
-    positive_eigenvectors = np.array([eigenvectors[i] for i in range(rank)])
+    positive_eigenvectors = eigenvectors[:, :rank]
 
     #Construct U with array broadcasting
     U = np.dot(A, np.dot(positive_eigenvectors, np.diag(1/positive_singular_values)))
@@ -23,14 +28,30 @@ def svd(A: np.array, error_tolerance: float=10**-50):
     return U, positive_singular_values, positive_eigenvectors.conj().T
 
 def truncated_svd(A: np.array, s: int):
-    U, singular_values, VH = svd(A)
+    U, singular_values, VH = np.linalg.svd(A)
+    print(singular_values)
 
     #U_truncated should be m x s, V_truncated should be n x s, and sigma_truncated should be s x s
     U_truncated = U[:, :s]
     singular_values_truncated = singular_values[:s]
-    V_truncated = VH[:, :s]
-    return U_truncated, singular_values_truncated, V_truncated.conj().T
+    VH_truncated = VH[:s, :]
+    return U_truncated, singular_values_truncated, VH_truncated
 
+def compress_image(image_file: str):
+    #Read the image as an array
+    image = imread(image_file)
+    if len(image.shape) > 2:
+        print("Image not grayscale...exiting")
+        exit(0)
+    U, singular_values, VH = truncated_svd(image, int(input(f"This image has {image.shape[0]} singular values. Enter the number of singular values to retain: ")))
+    compressed_matrix = np.dot(U, np.dot(np.diag(singular_values), VH))
+    data = Image.fromarray(compressed_matrix, mode='L')
+    output_file = f"{image_file.split('.')[0]}_compressed.jpg"
+    data.save(output_file)
+    print(f"Compressed image saved as {output_file}")
+
+
+'''
 A = np.array([[1, 2, 3], [3, 4, 5]])
 U, singular_values, VH = svd(A)
 print(f"U: \n{U}")
@@ -43,3 +64,6 @@ print(f"U_truncated: \n{U_truncated}")
 print(f"Singular Values Truncated: \n{singular_values_truncated}")
 print(f"VH_truncated: \n{VH_truncated}")
 print(f"Reconstructed Matrix: \n{np.dot(U_truncated, np.dot(np.diag(singular_values_truncated), VH_truncated))}")
+'''
+
+compress_image(sys.argv[1])
